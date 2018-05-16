43ed46ed332b6d439dc7ee85c5f4c85531810e85
Your Name <geekishmai@163.com>
2018-05-16 23:05:48 +0800

    Initial commit

---
+++ contain.js
@@ -0,0 +1,7 @@
+function setText() {
+    document.getElementById("playButton").style.textDecoration = "overline";
+
+}
+function cancelText() {
+    document.getElementById("playButton").style.textDecoration ="none";
+}
