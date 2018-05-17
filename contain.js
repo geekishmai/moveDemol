7d09c9d3cc68544624364aca5601317af5e51386
mai <geekishmai@163.com>
2018-05-18 00:10:57 +0800

    第二次

--- contain.js
+++ contain.js
@@ -5,3 +5,21 @@ function setText() {
 function cancelText() {
     document.getElementById("playButton").style.textDecoration ="none";
 }
+
+document.getElementById("playButton").onclick=function () {
+    move('.shadow')
+        .to(500, 200)
+        .rotate(180)
+        .scale(.5)
+        .set('background-color', '#FF0551')
+        .set('border-color', 'black')
+        .duration('3s')
+        .skew(50, -10)
+        .then()
+        .set('opacity', 0)
+        .duration('0.3s')
+        .scale(0.1)
+        .pop()
+        .end();
+};
+console.log("ojbk");
